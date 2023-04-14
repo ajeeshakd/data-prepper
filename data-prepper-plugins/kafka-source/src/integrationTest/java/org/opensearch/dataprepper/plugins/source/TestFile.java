b35917d5ac709e4d1e6e5932c63a9aa9735c0258
Ajeesh Gopalakrishnakurup <ajeesh.akd@gmail.com>
2023-04-14 19:14:13 +0530

    Kafka source test file initial commit Test File

---
+++ data-prepper-plugins/kafka-source/src/integrationTest/java/org/opensearch/dataprepper/plugins/source/TestFile.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.dataprepper.plugins.source;
+
+
+/**
+ * Generates comma-separated CSV records where each record is on a single line.
+ */
+class TestFile {
+
+}
